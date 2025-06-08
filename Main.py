771cabe24e75cf637bb7e5b878124ff3fadc0654
Pedro Silva <pedrosadultos@hotmail.com>
2025-06-08 20:06:19 -0300

    Terminando de modelar as tabelas

--- Main.py
+++ Main.py
@@ -6,11 +6,18 @@ if __name__ == "__main__":
     dao = DAO.DAO()
 
     strogonoff = Models.Refeicao(nome_refeicao="Strogonoff", descricao="Delicious beef strogonoff")
-    joao = Models.Usuario(nome="João", email="joao@hotmail.com", senha="123456", genero="Masculino", altura=180, peso=75)
+    arroz = Models.Alimento(nome_alimento="Arroz", unidade_padrao="xícara", calorias=200)
+    tomate = Models.Alimento(nome_alimento="Tomate", unidade_padrao="g", calorias=20)
+    carboidrato = Models.Nutriente(nome_nutriente="Carboidrato", unidade_medida="g")
+    proteina = Models.Nutriente(nome_nutriente="Proteína", unidade_medida="g")
+    gordura = Models.Nutriente(nome_nutriente="Gordura", unidade_medida="g")
+    carne = Models.Categoria_Alimento(nome_categoria="Carne")
+    picanha = Models.Alimento(nome_alimento="Picanha", unidade_padrao="g", calorias=300, categoria=carne)
 
-    # dao.adicionar_entidade(strogonoff)
-    # dao.adicionar_entidade(joao)
-    
-    dao.adicionar_vinculo(Models.Usuario, Models.Refeicao, 2, 2)
+
+    dao.adicionar_entidade(carne)
+    dao.adicionar_entidade(picanha)
+
+
 
     
--- Models.py
+++ Models.py
@@ -2,11 +2,34 @@ from DB import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
-usuario_refeicao = Table( # Pra fazer uma refeição customizada, primeiro cria uma refeição, depois bota na tabela associação.
-    'usuario_refeicao', Base.metadata,
-    Column('id_usuario', Integer, ForeignKey('usuario.id_usuario')),
-    Column('id_refeicao', Integer, ForeignKey('refeicao.id_refeicao'))
-)
+class AlimentoNutriente(Base):
+    __tablename__ = 'alimento_nutriente'
+
+    id_alimento = Column(Integer, ForeignKey('alimento.id_alimento'), primary_key=True)
+    id_nutriente = Column(Integer, ForeignKey('nutriente.id_nutriente'), primary_key=True)
+    quantidade_por_100un = Column(Integer, nullable=False)
+
+    alimento = relationship("Alimento", back_populates="alimento_nutrientes")
+    nutriente = relationship("Nutriente", back_populates="alimento_nutrientes")
+
+class RefeicaoAlimento(Base):
+    __tablename__ = 'refeicao_alimento'
+
+    id_refeicao = Column(Integer, ForeignKey('refeicao.id_refeicao'), primary_key=True)
+    id_alimento = Column(Integer, ForeignKey('alimento.id_alimento'), primary_key=True)
+    quantidade = Column(Integer, nullable=False)
+
+    refeicao = relationship("Refeicao", back_populates="refeicao_alimento")
+    alimento = relationship("Alimento", back_populates="refeicao_alimento")
+
+class UsuarioRefeicao(Base):
+    __tablename__ = 'usuario_refeicao'
+
+    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'), primary_key=True)
+    id_refeicao = Column(Integer, ForeignKey('refeicao.id_refeicao'), primary_key=True)
+
+    usuario = relationship("Usuario", back_populates="usuario_refeicao")
+    refeicao = relationship("Refeicao", back_populates="usuario_refeicao")
 
 class Usuario(Base):
     __tablename__ = 'usuario'
@@ -19,11 +42,7 @@ class Usuario(Base):
     altura = Column(Integer, nullable=False)
     peso = Column(Integer, nullable=False)
 
-    refeicao = relationship(
-        "Refeicao", 
-        secondary=usuario_refeicao,
-        back_populates="usuario"
-    )
+    usuario_refeicao = relationship("UsuarioRefeicao", back_populates="usuario")
 
 class Refeicao(Base):
     __tablename__ = 'refeicao'
@@ -32,11 +51,8 @@ class Refeicao(Base):
     nome_refeicao = Column(String(100), nullable=False)
     descricao = Column(Integer, nullable=False)
 
-    usuario = relationship(
-        "Usuario",
-        secondary=usuario_refeicao,
-        back_populates="refeicao"   
-    )
+    usuario_refeicao = relationship("UsuarioRefeicao", back_populates="refeicao")
+    refeicao_alimento = relationship("RefeicaoAlimento", back_populates="refeicao")
 
 class Alimento(Base):
     __tablename__ = 'alimento'
@@ -45,6 +61,12 @@ class Alimento(Base):
     nome_alimento = Column(String(100), nullable=False)
     unidade_padrao = Column(String(50), nullable=False)
     calorias = Column(Integer, nullable=False)
+
+    id_categoria = Column(Integer, ForeignKey('categoria_alimento.id_categoria'), nullable=False)
+
+    alimento_nutrientes = relationship("AlimentoNutriente", back_populates="alimento")
+    refeicao_alimento = relationship("RefeicaoAlimento", back_populates="alimento")
+    categoria = relationship("Categoria_Alimento", back_populates="alimento")
     
 class Nutriente(Base):
     __tablename__ = 'nutriente'
@@ -53,6 +75,8 @@ class Nutriente(Base):
     nome_nutriente = Column(String(100), nullable=False)
     unidade_medida = Column(String(50), nullable=False)
 
+    alimento_nutrientes = relationship("AlimentoNutriente", back_populates="nutriente")
+
 class Registro_Diario(Base):
     __tablename__ = 'registro_diario'
 
@@ -60,3 +84,12 @@ class Registro_Diario(Base):
     id_usuario = Column(Integer, nullable=False)
     data = Column(String(10), nullable=False)  # Formato YYYY-MM-DDq
     log = Column(String(500), nullable=False)
+
+class Categoria_Alimento(Base):
+    __tablename__ = 'categoria_alimento'
+
+    nome_categoria = Column(String(100), nullable=False)
+    id_categoria = Column(Integer, primary_key=True, autoincrement=True)
+    descricao = Column(String(500), nullable=True)
+
+    alimento = relationship("Alimento", back_populates="categoria")
